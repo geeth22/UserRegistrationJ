f48ed8928be394f2043e2c17b08427011c145bd4
geeth22 <geethchandra22@gmail.com>
2020-11-27 23:24:32 +0530

    added: pattern function to validate password

--- utility.js
+++ utility.js
@@ -2,8 +2,8 @@ var readlineSync = require('readline-sync');
 class UserRegistration {
     CheckPattern = () => {
         try{
-            let input = /^[1-9]{2}\s{0,1}[0-9]{10}$/
-        var UserName = readlineSync.question('Enter Phone Number ');
+            let input = /^[a-zA-Z0-9]{8,}$/
+        var UserName = readlineSync.question('Enter Password ');
         const pattern = input.exec(UserName);
         if (pattern) {
             console.log("Valid");
