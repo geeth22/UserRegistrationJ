71a216aaa47b2c67eedf815ef34396cac93885be
geeth22 <geethchandra22@gmail.com>
2020-11-27 23:08:40 +0530

    added: pattern function to valid first name

--- utility.js
+++ utility.js
@@ -1,1 +1,22 @@
-//Welcome to user registration
+var readlineSync = require('readline-sync');
+class UserRegistration {
+    CheckPattern = () => {
+        try{
+            let input = /^[A-Z]{1}[a-z]{2,}$/
+           // let Lname = /^[A-Z]{1}[a-z]{2,}$/
+        var UserName = readlineSync.question('Enter First Name? ');
+        const pattern = input.exec(UserName);
+        if (pattern) {
+            console.log("Valid");
+        }
+        else {
+            console.log("Not Valid");
+
+        }
+            }catch(e)
+        {
+            console.log(e)
+        }
+    }
+}
+module.exports = new UserRegistration();
